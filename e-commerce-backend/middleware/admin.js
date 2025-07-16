fb4bdbea7016840f28b5651de5ed40b28b4e779d
Arif Billah <ha661bibullah@gmail.com>
2025-07-16 14:08:02 +0600

    Added missing admin middleware

--- e-commerce-backend/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-// middleware/admin.js
-
-module.exports = function (req, res, next) {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ msg: "Access denied. Admins only." });
-  }
-  next();
-};
